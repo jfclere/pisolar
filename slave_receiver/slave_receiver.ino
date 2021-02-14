44065e97e69ad78547c64d62fb7be5b25d51f308
Jean-Frederic Clere <jfclere@gmail.com>
2021-02-14 10:28:16 +0100

    Add the TinyWireS from https://github.com/rambo/TinyWire/tree/rollback/TinyWireS

--- slave_receiver/slave_receiver.ino
+++ slave_receiver/slave_receiver.ino
@@ -1,58 +1,87 @@
 // Wire Slave Receiver
 
 
-#include <Wire.h>
+#include "TinyWireS.h"
 
 #define SHUTDOWNTIME 1000
 #define WAITFACTOR 60000
 
 bool up = true; // start it!!!
 int stopfor = 0;
-int count = 0;
+int val = 0;
+// int analogPin = ADC2;
+uint8_t c = 0;
+const int ledgreen = PB1;
+const int ledred = PB3;
+bool redon = false;
+bool greenon = false;
 
 void setup()
 {
-  Wire.begin(4);                // join i2c bus with address #4
-  Wire.onReceive(receiveEvent); // register event
-  Wire.onRequest(requestEvent); // interrupt handler for when data is wanted
-  pinMode(11, OUTPUT); // Relay to PI on 11
+  TinyWireS.begin(0x04);                // join i2c bus with address #4
+  TinyWireS.onReceive(receiveEvent); // register event
+  TinyWireS.onRequest(requestEvent); // interrupt handler for when data is wanted
+  pinMode(ledgreen, OUTPUT);
+  pinMode(ledred, OUTPUT);
 }
 
 void loop()
 {
+  TinyWireS_stop_check();
+  /* 
+  
   delay(100);
+  val = analogRead(analogPin);
   if (stopfor) {
     // We have just received a stop for the PI
     delay(SHUTDOWNTIME); // give time to stop.
   }
   if (up)
-    digitalWrite(11, HIGH);
+    digitalWrite(led, HIGH);
   else {
     // stop and wait for stopfor seconds 
-    digitalWrite(11, LOW);
+    digitalWrite(led, LOW);
     if (stopfor) {
       delay(stopfor * WAITFACTOR);
       stopfor = 0;
       up = true;
     }
   }
+  */
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany)
+void receiveEvent(uint8_t howMany)
 {
-  while(Wire.available())
+  if (howMany < 1)
+    return;
+  while(howMany--)
   {
-    char c = Wire.read(); // receive byte as a character
-    if (c) {
+    c = TinyWireS.receive(); // receive byte as a character
+    if (redon)
+      digitalWrite(ledred, LOW);
+    else
+      digitalWrite(ledred, HIGH);
+    /* if (c) {
       stopfor = c;
       up = false;
     }
-    count++;
+    */
   }
 }
 void requestEvent ()
 {
-  Wire.write(count);
+  TinyWireS.send(c);
+  if (greenon)
+      digitalWrite(ledgreen, LOW);
+    else
+      digitalWrite(ledgreen, HIGH);
+
+  /*
+  if (c)
+    TinyWireS.send(val/256);
+  else
+    TinyWireS.send(val%256);
+  */
 }
