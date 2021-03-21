b6db4b8f9560b72624b149d1826b1c773a66b955
Jean-Frederic Clere <jfclere@gmail.com>
2021-03-21 19:07:45 +0100

    Add another variante of the same ;-)

---
+++ charger_receiver/charger_receiver.ino
@@ -0,0 +1,136 @@
+// Wire Slave Receiver
+// Attiny45 controling a LiPo Battery and solar planel to power a PIzero
+
+
+#include "TinyWireS.h"
+#include "core_adc.h"
+
+
+#define SHUTDOWNTIME 30000UL
+#define WAITFACTOR 60000UL
+
+unsigned long stopfor = 0;
+int val = 0;
+int sending = 0; // to store val while sending it.
+
+uint8_t c = 0;
+
+// PB0 is SDA
+const int ledgreen = PB1; // Not a led in fact the optocoupler
+// PB2 is SCL
+const int ledred = PB3;
+const int analogPin = ADC_Input_ADC2; // PB4;
+// const int analogPin = ADC_Input_ADC0; // PB5 and RESET! = PROBLEMS!!!
+
+bool redon = false;
+bool ispion = false;
+bool start_conversion = true;
+
+/* 3V according to divisor (1000+220)/220 = 5.5454 and ref = 1.1V */
+/* 505/1024*1.1*5.5454 = 3.0805 V */
+/* according to my testa until around 440 = 2.621 V the USB is stable */ 
+#define BATCHARGED 700
+#define BATLOW 440
+void setup()
+{
+  TinyWireS.begin(0x04);                // join i2c bus with address #4
+  TinyWireS.onReceive(receiveEvent); // register event
+  TinyWireS.onRequest(requestEvent); // interrupt handler for when data is wanted
+  pinMode(ledgreen, OUTPUT);
+  pinMode(ledred, OUTPUT);
+}
+
+void loop()
+{
+  TinyWireS_stop_check();
+  if (start_conversion) {
+    start_conversion = false;
+    ADC_SetVoltageReference(ADC_Reference_Internal_1p1);
+    ADC_SetInputChannel(analogPin);
+    ADC_StartConversion();
+  } else {
+    /* ADC conversion in progress */
+    if(!ADC_ConversionInProgress()) {
+      /* we have a value, read it */
+      val = ADC_GetDataRegister();
+      // start_conversion = true;
+      if (val<BATCHARGED) {
+        digitalWrite(ledgreen, HIGH);
+        ispion = false;
+      } else {
+        digitalWrite(ledgreen, LOW);
+        ispion = true;
+      }
+      ADC_StartConversion();
+    }
+  }
+
+  /* do nothing until we don't know if we have enough battery */
+  if (!val)
+     return;
+
+  /* Not enough battery and off do nothing */
+  if (val<BATLOW && !ispion)
+     return;
+
+
+  /*
+  // stop and sleep.
+  if (stopfor) {
+    // We have just received a stop for the PI
+    delay(SHUTDOWNTIME); // give time to stop.
+    digitalWrite(ledgreen, HIGH);
+    ispion = false;
+    if (redon) {
+      digitalWrite(ledred, LOW);
+      redon = false;
+    } else {
+      digitalWrite(ledred, HIGH);
+      redon = true;
+    }
+    delay(stopfor * WAITFACTOR);
+    stopfor = 0;
+    val = 0;
+    return;
+  }
+
+  // Otherwise just switch on.
+  if (!ispion) {
+    digitalWrite(ledgreen, LOW);
+    ispion = true;
+  }
+  */
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(uint8_t howMany)
+{
+  if (howMany < 1)
+    return;
+  while(howMany--)
+  {
+    c = TinyWireS.receive(); // receive byte as a character
+    if (redon) {
+      digitalWrite(ledred, LOW);
+      redon = false;
+    } else {
+      digitalWrite(ledred, HIGH);
+      redon = true;
+    }
+    // c = 0 : read high, c = 1 : read low, otherwise PI sleeps for c.
+    if (c>1) {
+      stopfor = c;
+    }
+  }
+}
+void requestEvent ()
+{
+  // Send the low or high value
+  if (c) {
+    TinyWireS.send((uint8_t) (sending%256));
+  } else {
+    sending = val;
+    TinyWireS.send((uint8_t) (sending/256));
+  }
+}
