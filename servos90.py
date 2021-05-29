4319a04a58c255514b74121a5f570d81f9958683
Jean-Frederic Clere <jfclere@gmail.com>
2021-05-29 11:37:00 +0200

    Add middle positions for the 2 servos.

--- servos90.py
+++ servos90.py
@@ -8,28 +8,26 @@ import RPi.GPIO as GPIO
 import pigpio
 import time
  
-servo = 14
-#servo = 4
+servoh = 14
+servov = 4
  
 # more info at http://abyz.me.uk/rpi/pigpio/python.html#set_servo_pulsewidth
  
 pwm = pigpio.pi() 
-pwm.set_mode(servo, pigpio.OUTPUT)
+pwm.set_mode(servoh, pigpio.OUTPUT)
+pwm.set_mode(servov, pigpio.OUTPUT)
  
-pwm.set_PWM_frequency( servo, 50 )
- 
-print( "0 deg" )
-pwm.set_servo_pulsewidth( servo, 500 ) ;
-time.sleep( 10 )
+pwm.set_PWM_frequency( servoh, 50 )
+pwm.set_PWM_frequency( servov, 50 )
  
 print( "90 deg" )
-pwm.set_servo_pulsewidth( servo, 1500 ) ;
-time.sleep( 10 )
- 
-print( "180 deg" )
-pwm.set_servo_pulsewidth( servo, 2500 ) ;
+pwm.set_servo_pulsewidth( servoh, 1500 ) ;
+pwm.set_servo_pulsewidth( servov, 1500 ) ;
 time.sleep( 10 )
  
 # turning off servo
-pwm.set_PWM_dutycycle(servo, 0)
-pwm.set_PWM_frequency( servo, 0 )
+pwm.set_PWM_dutycycle(servoh, 0)
+pwm.set_PWM_frequency( servoh, 0 )
+
+pwm.set_PWM_dutycycle(servov, 0)
+pwm.set_PWM_frequency( servov, 0 )
