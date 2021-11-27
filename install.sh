c97d125046ae652a520d9766d0d160d073f2aa7f
Jean-Frederic Clere <jfclere@gmail.com>
2021-11-27 17:40:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # install git and checkout the repo
-# sudo apt-get install git cadaver libcamera-apps
+# sudo apt-get install git cadaver libcamera-apps python3-smbus
 # sudo raspi-config (activate camera and i2c)
 # copy and test the ssh keys
 # remove the password
