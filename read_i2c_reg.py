14cdc5b7d3b99c478953587479fdbd5e9dd790f5
Jean-Frederic Clere <jfclere@gmail.com>
2022-02-26 16:57:27 +0100

    improve i2c handling.

---
+++ read_i2c_reg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Tell ATtiny to read a register
+import sys
+import smbus
+import time
+from ctypes import c_short
+from ctypes import c_byte
+from ctypes import c_ubyte
+
+DEVICE = 0x04 # Default device I2C address
+
+bus = smbus.SMBus(1) # Rev 2 Pi, Pi 2 & Pi 3 uses bus 1
+                     # Rev 1 Pi uses bus 0
+def main():
+    reg = int(sys.argv[1])
+    reply = bus.read_i2c_block_data(DEVICE, reg, 2)
+    print(reply)
+
+
+if __name__ == "__main__":
+    main()
