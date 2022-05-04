4eb9e75ac20d7dfd4948424d01fa3e3f5bcc0d6b
Jean-Frederic Clere <jfclere@gmail.com>
2022-05-04 10:05:04 +0200

    Arrange the install logic to get it automatic.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-# install git and checkout the repo
-# sudo apt-get install git cadaver libcamera-apps python3-smbus
+# install git and other tools we need.
+sudo apt-get update
+sudo apt-get -y install git cadaver libcamera-apps python3-smbus
+# checkout the repo
+git clone https://github.com/jfclere/pisolar.git
 # sudo raspi-config (activate camera and i2c)
 # copy and test the ssh keys
 # remove the password
@@ -12,10 +15,10 @@
 
 sudo mkdir /var/log/journal
 sudo systemd-tmpfiles --create --prefix /var/log/journal
-sudo apt --autoremove purge rsyslog
-sudo cp journald.conf /etc/systemd/journald.conf
-sudo cp logrotate.conf /etc/logrotate.conf
-sudo cp solar.service /etc/systemd/system/
-sudo systemctl enable solar
+sudo apt -y --autoremove purge rsyslog
+sudo cp pisolar/journald.conf /etc/systemd/journald.conf
+sudo cp pisolar/logrotate.conf /etc/logrotate.conf
+sudo cp pisolar/image.service /etc/systemd/system/
+sudo systemctl enable image
 #wget -O bme280.py http://bit.ly/bme280py
 # a python3 version is in the current repo...
---
+++ preinstall.sh
@@ -0,0 +1,27 @@
+# mount | grep mmcblk
+# find boot and copy boot/wpa_supplicant.conf and touch boot/ssh
+# and copy the ssh pub keys.
+BOOT_DIR=`mount | grep mmcblk | grep boot | awk ' { print $3 } '`
+ROOT_DIR=`mount | grep mmcblk | grep rootfs | awk ' { print $3 } '`
+if [ -z $BOOT_DIR ]; then
+  echo "BOOT_DIR empty!"
+  exit 1
+fi
+if [ -z $ROOT_DIR ]; then
+  echo "ROOT_DIR empty!"
+  exit 1
+fi
+touch $BOOT_DIR/ssh
+cp wpa_supplicant.conf $BOOT_DIR/
+
+# copy the ssh key
+sudo mkdir $ROOT_DIR/home/pi/.ssh
+sudo chown 1000:1000 $ROOT_DIR/home/pi/.ssh
+sudo cp $HOME/.ssh/id_rsa.pub $ROOT_DIR/home/pi/.ssh/authorized_keys
+sudo chown 1000:1000 $ROOT_DIR/home/pi/.ssh/authorized_keys
+
+# Copy the install we will run at the first boot.
+sudo cp install.sh $ROOT_DIR/home/pi/
+sudo cp install.service $ROOT_DIR/lib/systemd/system
+sudo ln -s /lib/systemd/system/install.service $ROOT_DIR//etc/systemd/system/multi-user.target.wants
+
