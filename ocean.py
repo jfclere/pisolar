8ced7d4f35bd77ed6da2b56fa5c34162e0fb9448
Jean-Frederic Clere <jfclere@gmail.com>
2023-06-18 09:40:53 +0100

    Use nodeinfo instead local logic

--- ocean.py
+++ ocean.py
@@ -3,8 +3,10 @@
 
 import RPi.GPIO as GPIO
 import os
-import requests
 import time
+from nodeinfo import nodeinfo
+from readreg import readreg
+from writereg import writereg
 
 OCEANGPIO=23
 
@@ -13,35 +15,15 @@ GPIO.setwarnings(False)
 GPIO.setup(OCEANGPIO,GPIO.OUT)
 GPIO.output(OCEANGPIO,GPIO.LOW)
 
-f = open("/etc/machine-id", "r")
-machine_id = f.read().rstrip()
-# print("*" + machine_id + "*")
-filename = os.getenv("HOME")+"/.netrc"
-f = open(filename, "r")
-txt = f.readline()
-x = txt.split(" ")
-machinename = x[1].rstrip()
-# print("*" + machinename + "*")
-# loop 10 times testing network and server
-# somehow for about 2 minutes. 
-i = 0
-ison = False
-while i < 5:
-  # print("on")
-  r = requests.get('https://www.apache.org')
-  if (r.status_code == 200):
-    r = requests.get('https://' + machinename + '/machines/' + machine_id)
-    # print('https://' + machinename + '/machines/' + machine_id + '.ok')
-    if (r.status_code == 200):
-      GPIO.output(OCEANGPIO,GPIO.HIGH)
-      print("on")
-      ison = True
-    time.sleep(5)
-  if not ison:
-    print("off")
-    GPIO.output(OCEANGPIO,GPIO.LOW)
-  time.sleep(5)
-  i += 1
+myinfo = nodeinfo()
+if myinfo.read():
+  print("Failed")
+  exit()
 
-# end make sur to stop
+if myinfo.TIME_ACTIVE > 0:
+  GPIO.output(OCEANGPIO,GPIO.HIGH)
+  print("on")
+  time.sleep(60*myinfo.TIME_ACTIVE)
+
+# end make sure to stop
 GPIO.output(OCEANGPIO,GPIO.LOW)
