86b88d2e7cdc0bd6d0fd2186226a3e6b7fc7b34c
Jean-Frederic Clere <jfclere@gmail.com>
2024-02-18 18:10:40 +0000

    Ajust output.

--- grovemultigasv2.sh
+++ grovemultigasv2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 REMOTE_DIR=grovemultigasv2
 /usr/bin/echo "mkcol ${REMOTE_DIR}" >> /tmp/cmd.txt
-/home/pi/pisolar/grovemultigasv2.py > /tmp/temp.txt
-/usr/bin/echo "put /tmp/temp.txt ${REMOTE_DIR}/temp.txt" >> /tmp/cmd.txt
+/home/pi/pisolar/grovemultigasv2.py > /tmp/gas.txt
+/usr/bin/echo "put /tmp/gas.txt ${REMOTE_DIR}/gas.txt" >> /tmp/cmd.txt
 /usr/bin/cadaver https://jfclere.myddns.me/webdav/ < /tmp/cmd.txt
